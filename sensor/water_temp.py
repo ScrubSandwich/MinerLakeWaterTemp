2b85becf638c084de337f7855d34fc11ae2faa45
ScrubSandwich <jakem@usa.com>
2020-07-07 19:16:30 -0400

    Add output to logfile

--- sensor/water_temp.py
+++ sensor/water_temp.py
@@ -20,12 +20,18 @@ rate = 15 * MINUTE
 initial_time = 2 * MINUTE
 
 url = "https://us-central1-minerlakewatertemperature.cloudfunctions.net/api/addTemperature"
-
+log = "../log/log.txt"
 # Used for running 'git pull' to update the software
 updateCount = 0
 updateFrequency = 55
 
 status_code = -1
+
+def write_file(message):
+    f = open(log, "a")  # append mode 
+    f.write(message)
+    f.write("\n") 
+    f.close()
 
 def read_data():
     f = open(device_file, 'r')
@@ -35,17 +41,17 @@ def read_data():
     return lines
 
 def reboot():
-    print("Rebooting...")
+    write_file("Rebooting...")
     os.popen('sudo reboot')
 
 def update():
     try:
-        print("Checking for Update...\n")
+        write_file("Checking for Update...\n")
         git_pull_output = os.popen('git pull')
-        print(git_pull_output)
-        print("\nUpdate complete.")
+        write_file(git_pull_output)
+        write_file("\nUpdate complete.")
     except:
-        print("Update Failed")
+        write_file("Update Failed")
         #reboot()
 
 def read_temp():
@@ -61,9 +67,9 @@ def read_temp():
         return temp_f
 
 # Wait for wifi to connect
-print("Waiting for wifi to connect on boot...")
+write_file("Waiting for wifi to connect on boot...")
 time.sleep(initial_time)
-print("Finished waiting for wifi to connect...")
+write_file("Finished waiting for wifi to connect...")
 
 update()
 
@@ -75,11 +81,11 @@ while True:
         x = requests.post(url, data = {'tempF': temp_f})
         status_code = x.status_code
     except:
-        print("Cannot POST to server")
+        write_file("Cannot POST to server")
         reboot()
 
-    print("Water Temp: " + str(temp_f) + " degrees F")
-    print("CPU Temp: " + cpu_temp)
+    write_file("Water Temp: " + str(temp_f) + " degrees F")
+    write_file("CPU Temp: " + cpu_temp)
 
     updateCount = updateCount + 1
     if (updateCount == updateFrequency):
@@ -87,10 +93,10 @@ while True:
         reboot()
 
     if (status_code == 200):
-        print("Success")
+        write_file("Success")
     else:
-        print("Bad request")
+        write_file("Bad request")
         reboot()
-    print()
+    write_file("")
 
     time.sleep(rate)
